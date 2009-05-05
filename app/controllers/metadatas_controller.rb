bc16e24d9edd540dfaa82ed51595b7fa4578401b
Voronkov Andrey <voronkovaa@gmail.com>
2009-05-05 12:52:08 +1000

    Metadata resource added

---
+++ app/controllers/metadatas_controller.rb
@@ -0,0 +1,2 @@
+class MetadatasController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :metadatas
+
   map.resources :doctoral_conclusions
 
   map.resources :personal_datas
