faf6c430056658c4c76176600370cacb9faf5d27
Voronkov Andrey <voronkovaa@gmail.com>
2009-05-10 23:39:25 +1000

    start year added to date of birdth

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,3 +1,11 @@
 class PatientsController < ApplicationController
-  active_scaffold :patient
+  active_scaffold :patient do |config|
+    config.columns[:id].label = 'Номер карты'
+    config.columns[:date_of_birdth].label = 'Дата рождения'
+    config.columns[:date_of_birdth].description = "рождения дата"
+    config.label = 'Пациент'
+    config.list.columns = [:id, :date_of_birdth]
+    config.columns[:date_of_birdth].description = "рождения дата"
+    config.list.sorting = {:id => 'ASC'}
+  end
 end
--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,2 +1,5 @@
 module PatientsHelper
+  def date_of_birdth_form_column(record, input_name)
+    date_select :record, :date_of_birdth, :name => input_name, :start_year => 1900
+  end
 end
