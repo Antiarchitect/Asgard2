fa7e627894bf16ea8bc8d3d5cd55f995059c2534
Voronkov Andrey <voronkovaa@gmail.com>
2009-05-12 19:12:14 +1000

    some silly errors fixed

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,8 +1,5 @@
 class PatientsController < ApplicationController
   active_scaffold :patient do |config|
-#    config.list.columns.exclude :family_name, :first_name, :last_name
-#    config.list.columns << :first_and_last_name
-#    config.columns << :full_name
     @metadata = Metadata.find(:all, :conditions => ["model='patients'", "locale='ru'"])
     @metadata.each do |meta|
       config.columns[meta.name_in_model].label = meta.name_in_view
--- app/controllers/personal_datas_controller.rb
+++ app/controllers/personal_datas_controller.rb
@@ -1,3 +1,11 @@
 class PersonalDatasController < ApplicationController
-  active_scaffold :personal_data
+  active_scaffold :personal_data do |config|
+    config.list.columns.exclude :family_name, :first_name, :last_name
+    config.list.columns << :full_name
+    @metadata = Metadata.find(:all, :conditions => ["model='personal_datas'", "locale='ru'"])
+    @metadata.each do |meta|
+      config.columns[meta.name_in_model].label = meta.name_in_view
+      config.columns[meta.name_in_model].description = meta.description
+    end
+  end
 end
