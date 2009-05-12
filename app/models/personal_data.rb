7440d2858f6c846b64866e639a618b59bf3c3343
Voronkov Andrey <voronkovaa@gmail.com>
2009-05-12 18:45:36 +1000

    just stable need to add more metadata

--- app/models/personal_data.rb
+++ app/models/personal_data.rb
@@ -1,3 +1,7 @@
 class PersonalData < ActiveRecord::Base
   belongs_to :patient
+
+  def full_name
+    "#{self.family_name} #{self.first_name} #{self.last_name}"
+  end
 end
--- db/migrate/20090512015018_add_test_data.rb
+++ db/migrate/20090512015018_add_test_data.rb
@@ -1,6 +1,8 @@
 class AddTestData < ActiveRecord::Migration
   def self.up
     Metadata.delete_all
+
+    # for patients
     Metadata.create(
       :model => 'patients' ,
       :name_in_model => 'date_of_birdth',
@@ -13,6 +15,36 @@ class AddTestData < ActiveRecord::Migration
       :name_in_model => 'id',
       :name_in_view => 'Номер карты',
       :description => 'Номер карты пациента',
+      :locale => 'ru'
+    )
+    
+    # for personal_datas
+    Metadata.create(
+      :model => 'personal_datas' ,
+      :name_in_model => 'first_name',
+      :name_in_view => 'Имя',
+      :description => 'Имя пациента',
+      :locale => 'ru'
+    )
+    Metadata.create(
+      :model => 'personal_datas' ,
+      :name_in_model => 'last_name',
+      :name_in_view => 'Отчество',
+      :description => 'Отчество пациента',
+      :locale => 'ru'
+    )
+    Metadata.create(
+      :model => 'personal_datas' ,
+      :name_in_model => 'family_name',
+      :name_in_view => 'Фамилия',
+      :description => 'Фамилия пациента',
+      :locale => 'ru'
+    )
+    Metadata.create(
+      :model => 'personal_datas' ,
+      :name_in_model => 'name',
+      :name_in_view => 'ФИО',
+      :description => 'Фамилия, Имя и Отчество пациента',
       :locale => 'ru'
     )
   end
