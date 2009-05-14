a234cdf082a25288a3fac12d1836bade52dcc281
Voronkov Andrey <voronkovaa@gmail.com>
2009-05-14 19:49:25 +1000

    only existant columns a viewed in objective_data

--- app/controllers/objective_datas_controller.rb
+++ app/controllers/objective_datas_controller.rb
@@ -1,10 +1,12 @@
 class ObjectiveDatasController < ApplicationController
   active_scaffold :objective_data do |config|
+    config.list.columns = []
     @metadata = Metadata.find(:all, :conditions => ["model='objective_datas'", "locale='ru'"])
     @metadata.each do |meta|
       unless config.columns[meta.name_in_model].nil?
         config.columns[meta.name_in_model].label = meta.name_in_view
         config.columns[meta.name_in_model].description = meta.description
+        config.list.columns << meta.name_in_model
       end
     end
     config.label = 'Объективные данные пациента'
