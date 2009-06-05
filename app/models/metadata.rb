1986cc87175078fc149bfb89fbd3c3a7c1898c04
Voronkov Andrey <voronkovaa@gmail.com>
2009-06-05 15:19:05 +1000

    I don't understand Ruby yet...:(

--- app/models/metadata.rb
+++ app/models/metadata.rb
@@ -1,5 +1,5 @@
 class Metadata < ActiveRecord::Base
   def self.find_metadatas(model, locale)
-    self.find(:all, :conditions => ["model='#{model}' AND locale='#{locale}'"])
+    find(:all, :conditions => ["model='#{model}' AND locale='#{locale}'"])
   end
 end
