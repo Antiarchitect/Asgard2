92cd576935f30eb9328ce3c438c49e240e8234e2
Voronkov Andrey <voronkovaa@gmail.com>
2009-06-06 13:19:41 +1000

    authentication added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,18 @@ class ApplicationController < ActionController::Base
     config.list.per_page = 10
     config.theme = :blue
   end
+  
+  helper_method :current_user
+  
+  private
+
+  def current_user_session
+    return @current_user_session if defined?(@current_user_sesion)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
+  end
 end
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,7 +6,7 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      flash[:notice] = "Successfully created user session."
+      flash[:notice] = "Вход осуществлен успешно."
       redirect_to root_url
     else
       render :action => 'new'
@@ -14,9 +14,9 @@ class UserSessionsController < ApplicationController
   end
   
   def destroy
-    @user_session = UserSession.find(params[:id])
+    @user_session = UserSession.find
     @user_session.destroy
-    flash[:notice] = "Successfully destroyed user session."
+    flash[:notice] = "Выход осуществлен успешно."
     redirect_to root_url
   end
 end
