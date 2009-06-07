2a405e852c948ee807e6b44612ee01096adf2827
Voronkov Andrey <voronkovaa@gmail.com>
2009-06-07 13:06:50 +1000

    starting point to hell

--- app/controllers/metadatas_controller.rb
+++ app/controllers/metadatas_controller.rb
@@ -1,5 +1,14 @@
 class MetadatasController < ApplicationController
   active_scaffold :metadata do |config|
     config.label = 'Администрирование'
+
+    config.columns[:name_in_view].label = 'Название'
+    config.columns[:description].label = 'Описание'
+    config.columns[:unit].label = 'Единица измерения (необязательно)'
+    
+    config.create.columns = [:model, :name_in_view, :description, :unit]
+    config.list.columns = [:name_in_view, :description, :unit]
+    config.show.columns = [:name_in_view, :description, :unit]
+    config.update.columns = [:name_in_view, :description, :unit]
   end
 end
