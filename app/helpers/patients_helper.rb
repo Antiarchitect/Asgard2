a608985b5e1c8b5c9193a82916a6921358e1e0d3
Voronkov Andrey <voronkovaa@gmail.com>
2009-06-07 12:46:09 +1000

    date ranges fixed in patients and visits

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,5 +1,5 @@
 module PatientsHelper
   def date_of_birdth_form_column(record, input_name)
-    date_select :record, :date_of_birdth, :name => input_name, :start_year => 1900
+    date_select :record, :date_of_birdth, :name => input_name, :start_year => 1900, :end_year => Time.now.year
   end
 end
--- app/helpers/visits_helper.rb
+++ app/helpers/visits_helper.rb
@@ -1,2 +1,8 @@
 module VisitsHelper
+  def start_date_form_column(record, input_name)
+    date_select :record, :start_date, :name => input_name, :start_year => 1980, :end_year => Time.now.year
+  end
+  def end_date_form_column(record, input_name)
+    date_select :record, :end_date, :name => input_name, :start_year => 1980, :end_year => Time.now.year
+  end
 end
